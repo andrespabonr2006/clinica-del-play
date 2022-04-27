da609a025e20279db8a61ac38ae555a4b0f85ec8
conanguevon <conan@gmail.com>
2022-04-27 17:25:10 -0500

    conanguevon

---
+++ bonilla.js
@@ -0,0 +1,95 @@
+
+let producto={
+    nombre:"logitech g203",
+    estado:"nuevo",
+    precio:"99999",
+    foto:""
+}
+let producto2={
+    nombre:"play 5",
+    estado:"nuevo",
+    precio:"3000000",
+    foto:""
+}
+let producto3={
+    nombre:"xbox serie x",
+    estado:"nuevo",
+    precio:"2500000",
+    foto:""
+}
+let producto4={
+    nombre:"uncharted saga ",
+    estado:"usado",
+    precio:"360000",
+    foto:""
+}
+let producto5={
+    nombre:"nintendo switch",
+    estado:"nuevo",
+    precio:"1200000",
+    foto:""
+}
+let producto6={
+    nombre:"play 4",
+    estado:"usado",
+    precio:"800000",
+    foto:""
+}
+let producto7={
+    nombre:"xbox one",
+    estado:"usado",
+    precio:"700000",
+    foto:""
+}
+let producto8={
+    nombre:"combo gamer",
+    estado:"no sabemos",
+    precio:"100000",
+    foto:""
+}
+let producto9={
+    nombre:"mando de play 4",
+    estado:"usado",
+    precio:"120000",
+    foto:""
+}
+let producto10={
+    nombre:"3ds",
+    estado:"usada",
+    precio:"400000",
+    foto:""
+}
+let producto11={
+    nombre:"xbox 360",
+    estado:"usadisimo",
+    precio:"500000",
+    foto:""
+}
+let producto12={
+    nombre:"play 3",
+    estado:"usado",
+    precio:"550000",
+    foto:""
+}
+let producto13={
+    nombre:"mando de xbox one",
+    estado:"usado",
+    precio:"115000",
+    foto:""
+}
+let producto14={
+    nombre:"elden ring",
+    estado:"nuevo",
+    precio:"200000",
+    foto:""
+}
+
+
+let productos=Array(
+    producto,producto2,producto3,producto4,producto5,producto6,prodcuto7,producto8,producto9,producto10,producto11,prodcuto12,producto13,producto14
+    )
+console.log(productos)
+productos.forEach(function(producto){
+console.log(producto)
+console.log(producto.preoducto2)
+})
